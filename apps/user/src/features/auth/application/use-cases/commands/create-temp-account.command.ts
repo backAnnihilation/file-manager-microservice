d27ae86da4dce844273b1c06305b657b9be381b7
iKlimkin <ghbrot@gmail.com>
2024-08-25 11:08:35 +0300

    refactor use-cases

--- apps/user/src/features/auth/application/use-cases/commands/create-temp-account.command.ts
+++
@@ -1,5 +0,0 @@
-import { InputEmailDto } from '../../../api/models/auth-input.models.ts/password-recovery.types';
-
-export class CreateTemporaryAccountCommand {
-  constructor(public createDto: InputEmailDto) {}
-}
--- apps/user/src/features/auth/application/use-cases/commands/update-password-temporary-account.command.ts
+++
@@ -1,5 +0,0 @@
-import { RecoveryPassDto } from '../../../api/models/auth-input.models.ts/recovery.model';
-
-export class UpdatePassTempAccountCommand {
-  constructor(public updateDto: RecoveryPassDto) {}
-}
