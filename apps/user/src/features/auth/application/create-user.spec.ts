a8cb48ebe487074bef2f5af6a39b24e95b65eff8
kotonchiK <anton.mut05@gmail.com>
2024-08-26 13:56:35 +0200

    fix 1.1

--- apps/user/src/features/auth/application/create-user.spec.ts
+++ apps/user/src/features/auth/application/create-user.spec.ts
@@ -1,8 +1,6 @@
-import { initSettings } from '../../../../test/tools/initSettings';
 
 describe('Create-user-use-case', () => {
   beforeAll(async () => {
-    const settings = await initSettings();
   });
 
   it('', () => {
