58aae19a9d825c57c00825958015da9d95a77cb4
iKlimkin <ghbrot@gmail.com>
2024-09-06 15:36:12 +0300

    asd

--- apps/user/src/core/config/app-settings.ts
+++ apps/user/src/core/config/app-settings.ts
@@ -7,12 +7,12 @@ import { INestApplication } from '@nestjs/common';
 import { exceptionFilterSetup } from './exception-filter.setup';
 import { ConfigService } from '@nestjs/config';
 import { EnvironmentVariables } from './configuration';
-import { Environment } from '../../../../../libs/shared/environment.enum';
+import { Environment } from '@shared/environment.enum';
 
 export const applyAppSettings = (app: INestApplication) => {
   const currentENV = app.get(ConfigService<EnvironmentVariables>).get('ENV');
 
-  currentENV !== Environment.TESTING && app.setGlobalPrefix('api/v1');
+  // currentENV !== Environment.TESTING && app.setGlobalPrefix('api/v1');
   const corsSetup = {
     origin: [/localhost(:\d+)?$/],
     credentials: true,
